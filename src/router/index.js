0472608d7f1e8a7adaae17cbae17b4676ecab24e
Sandra <js4ydy@hotmail.com>
2022-03-15 15:23:08 +0800

    - change from hash mode to history mode (no more # in URL)

--- src/router/index.js
+++ src/router/index.js
@@ -1,62 +1,51 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import BeersView from "../views/BeersView.vue";
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/beers',
-    name: 'beers',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/BeersView.vue')
-  },
-  {
-    path: '/taproom',
-    name: 'taproom',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/TaproomView.vue')
-  },
-  {
-    path: '/brewery',
-    name: 'brewery',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/BreweryView.vue')
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    path: '/events',
-    name: 'events',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/EventsView.vue')
-  }
-]
+    {
+        path: "/",
+        name: "home",
+        component: HomeView,
+    },
+    {
+        path: "/beers/",
+        name: "beers",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: BeersView,
+    },
+    {
+        path: "/taproom",
+        name: "taproom",
+        component: () => import("../views/TaproomView.vue"),
+    },
+    {
+        path: "/brewery",
+        name: "brewery",
+        component: () => import("../views/BreweryView.vue"),
+    },
+    {
+        path: "/about",
+        name: "about",
+        component: () => import("../views/AboutView.vue"),
+    },
+    {
+        path: "/events",
+        name: "events",
+        component: () => import("../views/EventsView.vue"),
+    },
+];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
-  scrollBehavior (to, from, savedPosition) {
-    // return desired position
-    console.log(to, from, savedPosition);
-    return { top: 0 }
-  }
-})
+    history: createWebHistory(),
+    routes,
+    scrollBehavior(to, from, savedPosition) {
+        // return desired position
+        console.log(to, from, savedPosition);
+        return { top: 0 };
+    },
+});
 
-export default router
+export default router;
