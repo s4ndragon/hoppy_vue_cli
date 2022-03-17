340ca09f6f7c803a868cc0e5aacc0104ae75b317
Sandra <js4ydy@hotmail.com>
2022-03-17 17:18:52 +0800

    - use firebase to get info og HomeProducts
    >> HomeProduct's slider 90% (need to adjest the mobile width
    display

--- src/components/home/HomeProducts.vue
+++ src/components/home/HomeProducts.vue
@@ -1,42 +1,18 @@
 <template>
     <section class="home_products">
         <div class="slide_left">
-            <div class="slide_pic">
-                <img src="../../assets/home/products/columbus-ipa-can.png" />
-            </div>
-            <div class="slide_pic">
-                <img src="../../assets/home/products/columbus-creeper-can.png" />
-            </div>
-            <div class="slide_pic">
-                <img src="../../assets/home/products/palmetto-mango-ipa.png" />
+            <div class="slide_pic" v-for="productsInfo in productsInfos" :key="productsInfo.id">
+                <img :src="require(`../../assets/home/products/${productsInfo.id}.png`)" />
             </div>
             <a class="prev" @click="plusSlides(-1)">&#10094;</a>
             <a class="next" @click="plusSlides(1)">&#10095;</a>
         </div>
         <div class="slide_right">
-            <div class="slide_text">
-                <p>Pale orange, with grapefruit, pine, and tropical fruit aromas</p>
+            <div class="slide_text" v-for="productsInfo in productsInfos" :key="productsInfo.id">
+                <h3 class="highlight">{{ productsInfo.name }}</h3>
+                <h4>{{ productsInfo.title }}</h4>
                 <p>
-                    India Pale Ales were originally brewed to withstand long ocean voyages by increasing the quantity of
-                    hops and malt used in their Pale Ale predecessors. Our take on IPA pours pale orange with a balanced
-                    malt presence and finishes with a combination of grapefruit, pine, and tropical fruit notes from
-                    Pacific Northwest grown hops.
-                </p>
-            </div>
-            <div class="slide_text">
-                <p>You've been warned</p>
-                <p>
-                    Pale malts provide a stage for the huge hop presence. Creeper is our limited release Imperial IPA
-                    featuring an ever evolving blend of American hops.
-                </p>
-            </div>
-            <div class="slide_text">
-                <p>Juicy. Tropical. Hoppy.</p>
-                <p>
-                    Cascade, Centennial, and Amarillo hops contribute aromas of bright citrus and subtle melon to this
-                    American IPA. The addition of mango, a particularly juicy stone fruit, further accentuates the
-                    tropical character of the featured hops and balances the hop bitterness with a pleasant, fruity
-                    sweetness for your all-day drinking enjoyment. Set sail. Get tropical. Drink up.
+                    {{ productsInfo.description }}
                 </p>
             </div>
         </div>
@@ -44,10 +20,13 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
     data() {
         return {
             slideIndex: 1,
+            productsInfos: [],
         };
     },
 
@@ -69,9 +48,6 @@ export default {
                 slide_text[i].style.display = "none";
                 slide_text[i].style.opacity = "0";
             }
-            // for (i = 0; i < dots.length; i++) {
-            //     dots[i].className = dots[i].className.replace(" active", "");
-            // }
             slides_pic[this.slideIndex - 1].style.display = "block";
             slide_text[this.slideIndex - 1].style.display = "block";
             setTimeout(() => {
@@ -83,9 +59,34 @@ export default {
         plusSlides(n) {
             this.showSlides((this.slideIndex += n));
         },
+        getProductsInfo() {
+            let vm = this;
+            // fetch("https://hoppy-vue-cli-bfe34-default-rtdb.asia-southeast1.firebasedatabase.app/homeProducts.json")
+            //     .then(function (response) {
+            //         // 直接轉成JSON格式
+            //         return response.json();
+            //         // this.productsInfo = response.json();
+            //     })
+            //     .then(function (j) {
+            //         // `j`會是一個JavaScript物件
+            //         console.log(j);
+            //         vm.productsInfos = j;
+            //     })
+            //     .catch(function (err) {
+            //         // Error :(
+            //         console.log(err);
+            //     });
+            axios
+                .get("https://hoppy-vue-cli-bfe34-default-rtdb.asia-southeast1.firebasedatabase.app/homeProducts.json")
+                .then((response) => (vm.productsInfos = response.data));
+        },
     },
+
     mounted() {
-        this.showSlides(this.slideIndex);
+        this.getProductsInfo();
+        setTimeout(() => {
+            this.plusSlides(1);
+        }, 200);
     },
 };
 </script>
@@ -93,13 +94,13 @@ export default {
 <style scoped lang="scss">
 section {
     display: flex;
-    justify-content: center;
+    justify-content: space-evenly;
     align-items: center;
     position: relative;
     width: 95%;
     margin: auto;
     .slide_left {
-        width: 50%;
+        width: 40%;
         position: relative;
         .slide_pic {
             display: none;
@@ -107,48 +108,84 @@ section {
             transition: all 0.5s linear;
             img {
                 height: 80vh;
+                max-height: 450px;
             }
         }
         /* Next & previous buttons */
-    .prev,
-    .next {
-        cursor: pointer;
-        position: absolute;
-        top: 50%;
-        width: auto;
-        margin-top: -22px;
-        padding: 16px;
-        color: #77b329;
-        font-weight: bold;
-        font-size: 30px;
-        transition: 0.6s ease;
-        border-radius: 0 3px 3px 0;
-        user-select: none;
-    }
-    /* Position the "next button" to the right */
-    .prev {
-        left: 0;
-        border-radius: 3px 0 0 3px;
-    }
-    .next {
-        right: 0;
-    }
+        .prev,
+        .next {
+            cursor: pointer;
+            position: absolute;
+            top: 50%;
+            width: auto;
+            margin-top: -22px;
+            padding: 16px;
+            color: #77b329;
+            font-weight: bold;
+            font-size: 30px;
+            transition: 0.6s ease;
+            border-radius: 0 3px 3px 0;
+            user-select: none;
+        }
+        /* Position the "next button" to the right */
+        .prev {
+            left: 0;
+            border-radius: 3px 0 0 3px;
+        }
+        .next {
+            right: 0;
+        }
 
-    /* On hover, add a black background color with a little bit see-through */
-    .prev:hover,
-    .next:hover {
-        background-color: #77b329;
-        color: #ffffff;
-    }
+        /* On hover, add a black background color with a little bit see-through */
+        .prev:hover,
+        .next:hover {
+            background-color: #77b329;
+            color: #ffffff;
+        }
     }
     .slide_right {
-        width: 50%;
+        width: 40%;
         position: relative;
         .slide_text {
             background-color: #77b329;
+            // background-image: url(../../assets/home/products/slide_text_bg.png);
             padding: 10px 15px;
+            display: none;
+            color: #d3d3d3;
+            border-radius: 10px;
+            box-shadow: #3d3f436e 0px 3px 8px;
+            max-height: 400px;
+
+            .highlight {
+                background: url(../../assets/home/products/brush.png);
+                background-repeat: no-repeat;
+                background-size: 100% 95%;
+                padding: 8px 0;
+            }
+            h3 {
+                font-size: 36px;
+                margin: 0.5em 0 0 0;
+            }
+            p {
+                text-align: left;
+            }
         }
     }
-    
+}
+
+@media screen and (max-width: 768px) {
+    section {
+        flex-direction: column;
+        .slide_left {
+        .slide_pic {
+            img {
+                max-height: 300px;
+            }
+        }
+        }
+        .slide_right {
+            width: 80%;
+        }
+    }
 }
 </style>
